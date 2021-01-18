816db82f565fc273726a635867af8b8b6f9539c9
Victorgsf <victor.gabriel930@gmail.com>
2021-01-18 20:49:29 -0300

    Pagina_SignUp

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,12 +1,19 @@
 import React, { useState, useContext } from 'react'
+import axios from 'axios'
+import './SignUp.css'
 
 
 const SignUp = props => {
+    const [userData, setUserData] = useState({})
 
     return (
         <div>
             <h1>OLA MUNDO</h1>
+            <input placeholder="Local para escrita do nome" onChange={(e) => {setUserData({...userData , name : e.target.value})}} value={setUserData.name}></input>
+            <div className="teste"></div>
         </div>
+
+
     )
 }
 
