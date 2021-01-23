995f81543694d35dd8f722e8ac8bbb936884edb3
Bernardo Andrade <bpires99@gmail.com>
2021-01-23 19:23:58 -0300

    findCpfRoute

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,16 +2,6 @@ const express = require('express')
 const router = express.Router()
 const user = require('../models/user')
 
-router.get('/getAllUsers', (req, res) => {
-    user.find({})
-    .then(users => {
-        res.json(users)
-    })
-    .catch(err => {
-        res.json(err)
-    })
-})
-
 router.get('/login', (req, res) => {
     user.findOne({cpf: req.body.cpf})
     .then(user => {
@@ -32,6 +22,27 @@ router.post('/signup', (req, res) => {
     signup.save()
     .then(data => {
         res.json(data)
+    })
+    .catch(err => {
+        res.json(err)
+    })
+})
+
+router.get('/getAllUsers', (req, res) => {
+    user.find({})
+    .then(users => {
+        res.json(users)
+    })
+    .catch(err => {
+        res.json(err)
+    })
+})
+
+router.get('/getUserByCpf', (req, res) => {
+    user.findOne({cpf: req.body.cpf})
+    .then(user => {
+        if(user) res.json(user)
+        else res.json(null)
     })
     .catch(err => {
         res.json(err)
