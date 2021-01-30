967aa577a26baebcb698dadf83aeb7841630ab66
markimwrs@hotmail.com <markimwrs@hotmil.com>
2021-01-30 19:33:38 -0300

    Biks_pop_up

--- src/components/Bikes.jsx
+++ src/components/Bikes.jsx
@@ -7,19 +7,42 @@ import Nav from './navebar.jsx'
 
 
 const Bikes = props => {
-
     const [bikes, setBikes] = useState()
+    const userCpf = document.cookie.split(';').filter(item => item.includes('cpf'))[0].split('cpf=')[1]
+    const [showPopUp, setShowPopUp] = useState(false)
+    const [rents, setRents] =useState()
+    const [x, setX] = useState(false)
+
     useEffect(() => {
         axios.get('http://localhost:3001/getAllBikes')
             .then(res => {
                 setBikes(res.data)
             })
+        axios.post('http://localhost:3001/getAllRentsByCpf', {cpf: userCpf})
+        .then(async res => {
+            let aux = []
+            await res.data.map(rent => {
+                aux.push(rent.id)
+                if(aux.length == res.data.length){
+                    setRents(aux)
+                    setX(!x)
+                    console.log("passei aqui",aux);
+                }
+            })
+        })
     }, [])
+
+    
+    const newRent = () => {
+        //Mudar database
+        setShowPopUp(true);
+    }
+
 
     const renderCards = () => 
         bikes.map(bike => {
             return (
-                <BikesCards bikeData={bike}/>
+                <BikesCards bikeData={bike} newRent = {newRent} rents = {rents} x ={x}/>
             )
         })
     
@@ -27,6 +50,15 @@ const Bikes = props => {
     return (
         <Fragment>
             <Nav/>
+            {showPopUp && <div className = "pop-up-container">
+                <div className = "pop-up">
+                    <div className = "pop-up-text">
+                        <label>Your bike has been rented!</label>
+                        <i className = "fas fa check"></i>
+                    </div>
+                    <button onClick = {() => setShowPopUp(false)}>X</button>
+                </div>
+            </div>}
             <div className="bikes-cards">
                 {bikes && renderCards()}
             </div>
--- src/components/RentsCards.jsx
+++ src/components/RentsCards.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useContext, Fragment } from 'react'
 import Paper from '@material-ui/core/Paper'
 import { makeStyles } from '@material-ui/core/styles'
+import './BikesCards.css'
+import './bootstrap.css'
 
 
 const RentsCards = props => {
@@ -8,35 +10,44 @@ const RentsCards = props => {
 
     const useStyles = makeStyles((theme) => ({
         mainPaper: {
-            width: '40%',
+            width: '600px',
             height: 'fit-content',
-            padding: '5vw',
+            padding: '1vw',
             borderRadius: '25px',
-            backgroundColor: 'rgb(220, 220, 220)',
+            backgroundColor: 'rgb(255, 255, 255)',
             marginTop: '50px'
         }
     }))
 
     const classes = useStyles()
+    let date = rentData.date.split('T')[0]
+    let retrieveDate = (date.split('-')[2] === '30' || date.split('-')[2] === '31' ) ?
+     (`2021-02-0${7-(31-date.split('-')[2])}`) : (`2021-02-0${7+date.split('-')[2]}`)
 
     return (
     <Paper className={classes.mainPaper}>
-        <div className="bike-card-container">
-            <div className="bike-card-section">
-                <img style={{height: '90px'}} src='https://static.netshoes.com.br/produtos/bicicleta-ksw-aro-29-freios-a-disco-21-marchas-cambios-shimano/18/CGY-0024-018/CGY-0024-018_zoom1.jpg?ts=1580294446'></img>
+        <div  className="card mb-3 bike-card">
+            <div className="card-body">
+                <div className="">
+                    <img src='https://static.netshoes.com.br/produtos/bicicleta-ksw-aro-29-freios-a-disco-21-marchas-cambios-shimano/18/CGY-0024-018/CGY-0024-018_zoom1.jpg?ts=1580294446'
+                    className="img-thumbnail	mb-3 d-none d-sm-block"></img>
+                </div>
+                <dl>
+                    <div className="info1">
+                        <dt>Nº de série:</dt>
+                        <dd>{rentData.id}</dd>
+                        <dt>Data de aluguel:</dt>
+                        <dd>{date.split('T')[0]}</dd>
+                    </div>
+                    <div className="info2">
+                        <dt>Preco:</dt>
+                        <dd>R${rentData.price},00</dd>
+                        <dt>Data de entrega:</dt>
+                        <dd>{retrieveDate.split('T')[0]}</dd>
+                    </div> 
+                </dl>             
             </div>
-            <div className="bike-card-section">
-                <label>Nº de série</label>
-                <label>{rentData.id}</label>
-            </div>
-            <div className="bike-card-section">
-                <label>Preco</label>
-                <label>{rentData.price}</label>
-            </div>
-        </div>
-        
-        
-        {/* <img src='https://static.netshoes.com.br/produtos/bicicleta-ksw-aro-29-freios-a-disco-21-marchas-cambios-shimano/18/CGY-0024-018/CGY-0024-018_zoom1.jpg?ts=1580294446'></img> */}
+        </div> 
     </Paper>
   )
 }
